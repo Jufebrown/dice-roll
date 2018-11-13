faa77589015e60335fdb554ed330afed47408cdd
Jufebrown <jufebrown@gmail.com>
2018-11-13 11:22:44 -0600

    refactor to have diceRoll return object with results and total

--- diceRoll.js
+++ diceRoll.js
@@ -1,10 +1,19 @@
 const diceRoll = (numberOfDice, numberOfDieSides) => {
-  const result = 0;
-  for (let i = 0; i < numberOfDice; i++) {
-    result += rollDie(numberOfDieSides);    
+  const resultsArray = [];
+  const resultsObj = {};
+  if(numberOfDice > 0){
+    for (let i = 0; i < numberOfDice; i++) {
+      resultsArray.push(rollDie(numberOfDieSides));
+    }
+    resultsObj.results = resultsArray;
+    resultsObj.total = resultsArray.reduce(function(a,b){return a + b;}, 0);
+  } else {
+    resultsObj.results = [];
+    resultsObj.total = 0;
   }
+  return resultsObj;
 }
 
 const rollDie = (numberOfDieSides) => {
-  return Math.floor(Math.random() * numberOfDieSides) +1
+  return Math.floor(Math.random() * numberOfDieSides) +1;
 }
