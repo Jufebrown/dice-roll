f29d34202f0ce934549e2165993a6bfdc01b2452
Jufebrown <jufebrown@gmail.com>
2018-11-14 14:44:14 -0600

    removed unnecessary guard on 0 dice

--- diceRoll.js
+++ diceRoll.js
@@ -4,16 +4,13 @@ const diceRoll = (numberOfDice, numberOfDieSides) => {
   resultsObj.numberOfDice = numberOfDice;
   resultsObj.numberOfDieSides = numberOfDieSides;
   
-  if(numberOfDice > 0){
-    for (let i = 0; i < numberOfDice; i++) {
-      resultsArray.push(Math.floor(Math.random() * numberOfDieSides) +1);
-    }
-    resultsObj.results = resultsArray;
-    resultsObj.total = resultsArray.reduce(function(a,b){return a + b;}, 0);
-  } else {
-    resultsObj.results = [];
-    resultsObj.total = 0;
+  for (let i = 0; i < numberOfDice; i++) {
+    resultsArray.push(Math.floor(Math.random() * numberOfDieSides) +1);
   }
+  
+  resultsObj.results = resultsArray;
+  resultsObj.total = resultsArray.reduce(function(a,b){return a + b;}, 0);
+
   return resultsObj;
 }
 
